29ceb86466a596a4d24a72fa80b52ccc82e4cfe5
Jan Snasel <snaselj@gmail.com>
2024-11-07 09:10:29 +0000

    fix: Job args

--- jobs/testing_job.py
+++ jobs/testing_job.py
@@ -5,7 +5,7 @@ class MemoryIntensiveJob(Job):
     class Meta:
         description = "Job that allocates approximately 2GB of memory for testing purposes."
 
-    def run(self, data, commit):
+    def run(self):
         # Allocate a list with 2^28 elements, each being a 1-byte string
         ["a"] * (2**28)
         self.log_info("Allocated approximately 2GB of memory.")

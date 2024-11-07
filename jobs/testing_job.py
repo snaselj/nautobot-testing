b6f2f0cdfec59ea043834463c729f8a95d0a1937
Jan Snasel <snaselj@gmail.com>
2024-11-07 09:11:23 +0000

    fix: Logger

--- jobs/testing_job.py
+++ jobs/testing_job.py
@@ -8,5 +8,5 @@ class MemoryIntensiveJob(Job):
     def run(self):
         # Allocate a list with 2^28 elements, each being a 1-byte string
         ["a"] * (2**28)
-        self.log_info("Allocated approximately 2GB of memory.")
+        self.logger.info("Allocated approximately 2GB of memory.")
         # Perform any additional operations as needed
